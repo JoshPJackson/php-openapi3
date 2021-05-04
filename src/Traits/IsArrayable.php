f788813b845975c6d65d821263bf1c7a4393a6a4
newicon_josh <josh.jackson@newicon.net>
2021-05-04 16:41:29 +0100

    Changed way of setting data

--- src/Traits/IsArrayable.php
+++ src/Traits/IsArrayable.php
@@ -2,32 +2,34 @@
 
 namespace JoshPJackson\OpenApi\Traits;
 
+use Exception;
+
 trait IsArrayable
 {
-    /**
-     * @return array
-     * @throws \Exception
-     */
-    public function toArray() : array
-    {
-        if (method_exists($this, 'validate')) {
-            if (!$this->validate()) {
-                throw new \Exception(get_class($this) . ' missing one of more required fields');
-            }
-        }
+	/**
+	 * @return array
+	 * @throws Exception
+	 */
+	public function toArray(): array
+	{
+		if (method_exists($this, 'validate')) {
+			if (!$this->validate()) {
+				throw new Exception(get_class($this) . ' missing one of more required fields');
+			}
+		}
 
-        foreach (get_object_vars($this) as $name => $value) {
-            if ($name != 'requiredFields') {
-                if (!empty($value)) {
-                    if (!is_object($value)) {
-                        $jsonArray[$name] = $value;
-                    } else {
-                        $jsonArray[$name] = json_decode($value->jsonSerialize());
-                    }
-                }
-            }
-        }
+		foreach (get_object_vars($this) as $name => $value) {
+			if ($name != 'requiredFields') {
+				if (!empty($value)) {
+					if (!is_object($value)) {
+						$jsonArray[$name] = $value;
+					} else {
+						$jsonArray[$name] = $value->toArray();
+					}
+				}
+			}
+		}
 
-        return $jsonArray;
-    }
+		return $jsonArray;
+	}
 }
